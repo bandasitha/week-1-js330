c26f14b0f5bcac7eb00334559dba42e1f8c23323
ambz <a.bandasith@gmail.com>
2022-07-12 18:42:01 -0700

    adding movie endpoints

---
+++ dataInterface/movies.js
@@ -0,0 +1,21 @@
+module.exports = {}
+
+module.exports.getAll = () => {
+    return []
+}
+
+module.exports.getbyId = (movieId) => {
+    return []
+}
+
+module.exports.deleteById = (movieId) => {
+    return []
+}
+
+module.exports.updateById = (movieId, newObj) => {
+    return []
+}
+
+module.exports.create = (newObj) => {
+    return {}
+}
